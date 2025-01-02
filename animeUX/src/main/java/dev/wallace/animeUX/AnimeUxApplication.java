b48db25a68b5201d228a8ed068ba2ceaf5ae2367
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-01-02 13:23:22 -0300

    first commit

---
+++ animeUX/src/main/java/dev/wallace/animeUX/AnimeUxApplication.java
@@ -0,0 +1,13 @@
+package dev.wallace.animeUX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeUxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeUxApplication.class, args);
+	}
+
+}
---
+++ animeUX/src/test/java/dev/wallace/animeUX/AnimeUxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wallace.animeUX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeUxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
