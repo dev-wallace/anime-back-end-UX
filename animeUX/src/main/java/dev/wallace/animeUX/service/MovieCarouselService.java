0d0d52bc90bc75e0887e8218e82176aa31b33453
yokaithefato <wallacewwc2015wwk@gmail.com>
2025-01-08 12:56:19 -0300

    Add MovieCarouselService for managing movie carousel functionality

---
+++ animeUX/src/main/java/dev/wallace/animeUX/service/MovieCarouselService.java
@@ -0,0 +1,42 @@
+package dev.wallace.animeux.service;
+
+import org.springframework.stereotype.Service;
+
+import dev.wallace.animeux.dto.MovieCarouselDTO;
+import dev.wallace.animeux.model.MovieCarousel;
+import dev.wallace.animeux.repository.MovieCarouselRepository;
+
+@Service
+public class MovieCarouselService {
+
+    private final MovieCarouselRepository movieCarouselRepository;
+
+    public MovieCarouselService(MovieCarouselRepository movieCarouselRepository) {
+        this.movieCarouselRepository = movieCarouselRepository;
+    }
+
+
+    public MovieCarouselDTO  createMovieCarousel(MovieCarouselDTO movieCarouselDTO) {
+
+       MovieCarousel movieCarousel = new MovieCarousel();
+
+         movieCarousel.setTitle(movieCarouselDTO.getTitle());
+        movieCarousel.setDescription(movieCarouselDTO.getDescription());
+        movieCarousel.setThumbnail(movieCarouselDTO.getThumbnail());
+        movieCarousel.setBackgroundImage(movieCarouselDTO.getBackgroundImage());
+        movieCarousel.setLogo(movieCarouselDTO.getLogo());
+
+        movieCarouselRepository.save(movieCarousel);
+
+        return new MovieCarouselDTO(
+        movieCarousel.getTitle(),
+        movieCarousel.getDescription(),
+        movieCarousel.getThumbnail(),
+        movieCarousel.getBackgroundImage(),
+        movieCarousel.getLogo());
+        
+    }
+
+     
+
+}
